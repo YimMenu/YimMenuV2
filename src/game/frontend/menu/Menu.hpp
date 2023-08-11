84f912cb344b0008a01e98b95c3b2468c647e925
Andreas Maerten <24669514+Yimura@users.noreply.github.com>
2023-08-11 22:51:23 +0200

    Initial commit

---
+++ src/game/frontend/menu/Menu.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "game/frontend/GUI.hpp"
+
+namespace NewBase::Menu
+{
+	extern void Main();
+}
---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+#include "common.hpp"
+#include "core/filemgr/FileMgr.hpp"
+#include "core/hooking/Hooking.hpp"
+#include "core/memory/ModuleMgr.hpp"
+#include "core/renderer/Renderer.hpp"
+#include "game/frontend/GUI.hpp"
+#include "game/pointers/Pointers.hpp"
+
+
+namespace NewBase
+{
+	DWORD Main(void*)
+	{
+		const auto documents = std::filesystem::path(std::getenv("USERPROFILE")) / "Documents";
+		FileMgr::Init(documents / "HellBase");
+
+		LogHelper::Init("henlo", FileMgr::GetProjectFile("./cout.log").Path());
+
+		if (!ModuleMgr.LoadModules())
+			goto unload;
+		if (!Pointers.Init())
+			goto unload;
+		if (!Renderer::Init())
+			goto unload;
+		GUI::Init();
+		Hooking::Init();
+
+		while (g_Running)
+		{
+			std::this_thread::sleep_for(100ms);
+		}
+
+	unload:
+		Hooking::Destroy();
+		Renderer::Destroy();
+		LogHelper::Destroy();
+
+		CloseHandle(g_MainThread);
+		FreeLibraryAndExitThread(g_DllInstance, EXIT_SUCCESS);
+
+		return EXIT_SUCCESS;
+	}
+}
+
+BOOL WINAPI DllMain(HINSTANCE dllInstance, DWORD reason, void*)
+{
+	using namespace NewBase;
+
+	DisableThreadLibraryCalls(dllInstance);
+
+	if (reason == DLL_PROCESS_ATTACH)
+	{
+		g_DllInstance = dllInstance;
+
+		g_MainThread = CreateThread(nullptr, 0, Main, nullptr, 0, &g_MainThreadId);
+	}
+	return true;
+}

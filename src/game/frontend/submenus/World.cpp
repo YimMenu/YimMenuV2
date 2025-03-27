27baa8501ab1c42af8d6bbab2048b647fd22ee07
unknown <27909321+oakboat@users.noreply.github.com>
2025-03-27 09:08:27 +0800

    feat(Remove LSC Restrictions): Removes Los Santos Customs vehicle modification restrictions

--- src/game/frontend/submenus/World.cpp
+++ src/game/frontend/submenus/World.cpp
@@ -8,6 +8,7 @@ namespace YimMenu::Submenus
 	{
 		auto mainGroup = std::make_shared<Category>("Spawners");
 		auto spawnGroup = std::make_shared<Group>("Vehicle");
+		auto modsGroup  = std::make_shared<Group>("Modifications");
 
 		spawnGroup->AddItem(std::make_shared<StringCommandItem>("vehmodelname"_J));
 		spawnGroup->AddItem(std::make_shared<BoolCommandItem>("spawninvehicle"_J));
@@ -16,7 +17,10 @@ namespace YimMenu::Submenus
 		spawnGroup->AddItem(std::make_shared<ConditionalItem>("usecustomlicenseplate"_J, std::make_shared<StringCommandItem>("customlicenseplate"_J)));
 		spawnGroup->AddItem(std::make_shared<CommandItem>("spawnvehicle"_J));
 
+		modsGroup->AddItem(std::make_shared<BoolCommandItem>("lsccustomsbypass"_J));
+
 		mainGroup->AddItem(spawnGroup);
+		mainGroup->AddItem(modsGroup); 
 
 		AddCategory(std::move(mainGroup));
 	}

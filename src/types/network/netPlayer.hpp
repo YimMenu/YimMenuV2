48ce6bc146bde79a277e990a90f81698a295e4da
AAA_ALR <b1745299937@126.com>
2025-04-10 21:17:49 +0800

    format

--- src/types/network/netPlayer.hpp
+++ src/types/network/netPlayer.hpp
@@ -31,7 +31,7 @@ namespace rage
 
 		char pad_0008[8];                                //0x0008
 		CNonPhysicalPlayerData* m_NonPhysicalPlayer;     //0x0010
-		char pad_0018[0x40];							 //0x0018
+		char pad_0018[0x40];                             //0x0018
 		uint32_t m_MessageId;                            //0x0048
 		char pad_004C[4];                                //0x004C
 		uint8_t m_ActiveIndex;                           //0x0060

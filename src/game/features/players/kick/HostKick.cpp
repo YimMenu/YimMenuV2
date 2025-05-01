dca68c09f1c796491c9e8c5de5e59320aa40fb80
latiao-1337 <146958060+latiao-1337@users.noreply.github.com>
2025-05-01 11:37:50 +0000

    add hostkick

---
+++ src/game/features/players/kick/HostKick.cpp
@@ -0,0 +1,18 @@
+#include "game/commands/PlayerCommand.hpp"
+#include "game/backend/NativeHooks.hpp"
+#include "game/gta/Natives.hpp"
+
+namespace YimMenu::Features
+{
+	class HostKick : public PlayerCommand
+	{
+		using PlayerCommand::PlayerCommand;
+		virtual void OnCall(Player player) override
+		{
+			NETWORK::NETWORK_SESSION_KICK_PLAYER(player.GetId());
+		}
+
+	};
+
+	static HostKick _HostKick{"HKick", "Host Kick", "Only available as host and the target is unblockable"};
+}

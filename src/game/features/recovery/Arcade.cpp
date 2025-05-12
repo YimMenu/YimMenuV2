1c521d102f7692e079924125c14ee29d89fdf8aa
Felix Wehrfeld <felix@wehrfeld.de>
2025-05-12 13:05:45 +0200

    add arcade safe payout

---
+++ src/game/features/recovery/Arcade.cpp
@@ -0,0 +1,33 @@
+#include "core/commands/LoopedCommand.hpp"
+#include "game/gta/Stats.hpp"
+#include <ctime>
+using namespace std;
+
+namespace YimMenu::Features
+{
+	class Arcade : public LoopedCommand
+	{
+		using LoopedCommand::LoopedCommand;
+        bool running = false;
+        int time1 = 1;
+
+		virtual void OnTick() override
+		{
+			time_t timestamp;
+			time(&timestamp);
+            if (time1 + 5 < timestamp) //inefficient because of only 5k
+            {
+            time1 = timestamp;
+            running = true;
+            Stats::SetInt("MPX_ARCADE_PAY_TIME_LEFT", 1000); //Triggers instant payout and removes payout timeout (int Time is in milliseconds)
+            }
+		}
+
+        virtual void OnDisable() override
+        {
+            running = false;
+        }
+	};
+
+	static Arcade _Arcade{"arcadesafe", "Trigger arcade safe payout", "Triggers arcade safe payout"};
+}

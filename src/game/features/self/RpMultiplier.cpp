8482a54f0a82de190616f413c2e4011a2e4790d6
Felix Wehrfeld <felix@wehrfeld.de>
2025-05-12 13:06:06 +0200

    added rp multiplier

---
+++ src/game/features/self/RpMultiplier.cpp
@@ -0,0 +1,28 @@
+#include "game/backend/Tunables.hpp"
+#include "core/commands/IntCommand.hpp"
+#include "core/commands/Command.hpp"
+
+namespace YimMenu::Features
+{
+	static IntCommand _RpMultiplierInput{"rpmultiplierinput", "RP Multiplier Input", "RP Mulitplier (< 0 = 0 -> No RP)", std::nullopt, std::nullopt, 1};
+	
+	class RPMultiplier : public Command
+	{
+		using Command::Command;
+
+		virtual void OnCall() override
+		{
+			int state = _RpMultiplierInput.GetState();
+			if (state < 0)
+			{
+				state = 0;
+			}
+
+			// int rp1 = *Tunables::GetTunable("XP_MULTIPLIER"_J).As<float*>();
+			// LOG(INFO) << "Debug: [ " << rp1 << " ]";
+			*Tunables::GetTunable("XP_MULTIPLIER"_J).As<float*>() = state;
+		}
+	};
+
+	static RPMultiplier _RPMultiplier{"rpmultiplier", "Set RP Multiplier", "Multiplies RP by the given value"};
+}

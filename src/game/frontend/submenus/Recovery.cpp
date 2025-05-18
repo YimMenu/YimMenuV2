11dc7cd01468057c7f61d11eb4757b38acb147a9
felixwehrfeld <felix.dienst@yahoo.de>
2025-05-19 00:04:49 +0200

    removed Nightclub and arcade safe payout because of https://github.com/YimMenu/YimMenuV2/pull/299#discussion_r2094595284. Moved RPMultiplier to Recovery general.

--- src/game/frontend/submenus/Recovery.cpp
+++ src/game/frontend/submenus/Recovery.cpp
@@ -11,19 +11,13 @@ namespace YimMenu::Submenus
 	Recovery::Recovery() :
 	    Submenu::Submenu("Recovery")
 	{
-		auto shopping  = std::make_shared<Category>("Shopping");
-		auto missions  = std::make_shared<Category>("Missions");
-		auto nightClub = std::make_shared<Category>("Nightclub");
-		auto arcade    = std::make_shared<Category>("Arcade");
-		auto casino    = std::make_shared<Category>("Casino");
-		auto rp        = std::make_shared<Category>("RP");
+		auto shopping = std::make_shared<Category>("Shopping");
+		auto missions = std::make_shared<Category>("Missions");
+		auto casino   = std::make_shared<Category>("Casino");
 
-		auto vehiclesGroup  = std::make_shared<Group>("Vehicles");
-		auto generalGroup   = std::make_shared<Group>("General");
-		auto nightClubGroup = std::make_shared<Group>("NightclubSafe");
-		auto arcadeGroup    = std::make_shared<Group>("ArcadeSafe");
-		auto casinoGroup    = std::make_shared<Group>("CasinoRigSlotMachines");
-		auto rpGroup        = std::make_shared<Group>("RP");
+		auto vehiclesGroup = std::make_shared<Group>("Vehicles");
+		auto generalGroup  = std::make_shared<Group>("General");
+		auto casinoGroup   = std::make_shared<Group>("CasinoRigSlotMachines");
 
 
 		vehiclesGroup->AddItem(std::make_shared<BoolCommandItem>("dlcvehicles"_J));
@@ -40,28 +34,17 @@ namespace YimMenu::Submenus
 
 		generalGroup->AddItem(std::make_shared<BoolCommandItem>("playallmissionssolo"_J));
 		generalGroup->AddItem(std::make_shared<CommandItem>("forcelaunchheist"_J));
-
-		nightClubGroup->AddItem(std::make_shared<BoolCommandItem>("nightclubsafe"_J));
-
-		arcadeGroup->AddItem(std::make_shared<BoolCommandItem>("arcadesafe"_J));
+		generalRGroup->AddItem(std::make_shared<IntCommandItem>("rpmultiplierinput"_J));
+		generalGroup->AddItem(std::make_shared<CommandItem>("rpmultiplier"_J));
 
 		casinoGroup->AddItem(std::make_shared<BoolCommandItem>("casinomanipulaterigslotmachines"_J));
 
-		rpGroup->AddItem(std::make_shared<IntCommandItem>("rpmultiplierinput"_J));
-		rpGroup->AddItem(std::make_shared<CommandItem>("rpmultiplier"_J));
-
 		shopping->AddItem(vehiclesGroup);
 
-		nightClub->AddItem(nightClubGroup);
-		arcade->AddItem(arcadeGroup);
 		missions->AddItem(generalGroup);
 		casino->AddItem(casinoGroup);
-		rp->AddItem(rpGroup);
 		AddCategory(std::move(shopping));
 		AddCategory(std::move(missions));
-		AddCategory(std::move(nightClub));
-		AddCategory(std::move(arcade));
-		AddCategory(std::move(rp));
 		AddCategory(std::move(casino));
 		AddCategory(BuildStatEditorMenu());
 		AddCategory(BuildTransactionsMenu());

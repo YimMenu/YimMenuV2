d367f29757c3bb26bffd89e6c8562a7ccd6c3c3f
felixwehrfeld <felix.dienst@yahoo.de>
2025-05-18 19:22:46 +0200

    fix https://github.com/YimMenu/YimMenuV2/pull/299#discussion_r2094338633 and https://github.com/YimMenu/YimMenuV2/pull/299#discussion_r2094338444

--- src/game/features/recovery/Casino.cpp
+++ src/game/features/recovery/Casino.cpp
@@ -1,25 +1,35 @@
 #include <random>
 #include <set>
 #include "game/gta/Natives.hpp"
-#include "core/commands/BoolCommand.hpp"
+#include "core/commands/LoopedCommand.hpp"
 #include "game/gta/ScriptLocal.hpp"
+#include "game/backend/Self.hpp"
 
 
 
 
 namespace YimMenu::Features
 {
-	class Casino : public BoolCommand
+	class CasinoManipulateRigSlotMachines : public LoopedCommand
 	{
-		using BoolCommand::BoolCommand;
+		using LoopedCommand::LoopedCommand;
 
         int slots_random_results_table = 1348;
         std::set<int> slots_blacklist = {9, 21, 22, 87, 152};
 
-		virtual void OnEnable() override
+		virtual void OnTick() override
 		{
-            bool needs_run = false;
-            if (SCRIPT::GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH("casino_slots"_J)) {
+            if (Scripts::SafeToModifyFreemodeBroadcastGlobals() && SCRIPT::GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH("casino_slots"_J)) 
+            {
+                Player casinoSlotsScriptHostPlayer = NETWORK::NETWORK_GET_HOST_OF_SCRIPT("casino_slots", -1, 0);
+                auto casinoSlotsScriptHostPlayerId = casinoSlotsScriptHostPlayer.GetId();
+                auto selfPlayerId = Self::GetPlayer().GetId();
+                if (casinoSlotsScriptHostPlayerId != selfPlayerId)
+                {
+                    Scripts::ForceScriptHost(Scripts::FindScriptThread("casino_slots"_J));
+                }
+
+                bool needs_run = false;
                 for (int slots_iter = 3; slots_iter <= 196; ++slots_iter) {
                     if (!slots_blacklist.contains(slots_iter)) {
                         if (*ScriptLocal("casino_slots"_J, slots_random_results_table + slots_iter).As<int*>() != 6) {
@@ -40,8 +50,16 @@ namespace YimMenu::Features
 
         virtual void OnDisable() override
         {
-            if (SCRIPT::GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH("casino_slots"_J))
+            if (Scripts::SafeToModifyFreemodeBroadcastGlobals() && SCRIPT::GET_NUMBER_OF_THREADS_RUNNING_THE_SCRIPT_WITH_THIS_HASH("casino_slots"_J))
             {
+                Player casinoSlotsScriptHostPlayer = NETWORK::NETWORK_GET_HOST_OF_SCRIPT("casino_slots", -1, 0);
+                auto casinoSlotsScriptHostPlayerId = casinoSlotsScriptHostPlayer.GetId();
+                auto selfPlayerId = Self::GetPlayer().GetId();
+                if (casinoSlotsScriptHostPlayerId != selfPlayerId)
+                {
+                    Scripts::ForceScriptHost(Scripts::FindScriptThread("casino_slots"_J));
+                }
+
                 for (int slots_iter = 3; slots_iter <= 196; ++slots_iter) {
                     if (!slots_blacklist.contains(slots_iter)) {
                         int slot_result = 6;
@@ -54,6 +72,6 @@ namespace YimMenu::Features
         }
 	};
 
-    static Casino _Casino{"rigslotmachines", "Rig Slot Machines", "Rig Slot Machines"};
+    static CasinoManipulateRigSlotMachines _CasinoManipulateRigSlotMachines{"casinomanipulaterigslotmachines", "Manipulate Rig Slot Machines", "Lets you win the Rig Slot Machines every time"};
 
 }

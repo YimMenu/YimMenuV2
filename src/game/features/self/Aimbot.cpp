4794baec6efea9fab9f08a707cc7489c42bce29b
felixwehrfeld <felix.dienst@yahoo.de>
2025-05-28 00:21:11 +0200

    fix: aimbot not switching to new targets

--- src/game/features/self/Aimbot.cpp
+++ src/game/features/self/Aimbot.cpp
@@ -1,16 +1,40 @@
 #include "core/commands/BoolCommand.hpp"
 #include "game/pointers/Pointers.hpp"
+#include "core/commands/LoopedCommand.hpp"
+#include "game/gta/Pools.hpp"
+#include "game/backend/Self.hpp"
+#include "game/gta/Natives.hpp"
+
+#include <set>
 
 namespace YimMenu::Features
 {
-	class Aimbot : public BoolCommand
+	class Aimbot : public LoopedCommand
 	{
-		using BoolCommand::BoolCommand;
+		bool running = false;
+		using LoopedCommand::LoopedCommand;
+
+		std::set<int> killed_peds = {  };
 
 		virtual void OnEnable() override
 		{
 			Pointers.ShouldNotTargetEntityPatch->Apply();
 			Pointers.GetAssistedAimTypePatch->Apply();
+		}
+
+		virtual void OnTick() override
+		{
+			if (running) return;
+			running = true;
+			for (Ped ped : Pools::GetPeds())
+			{
+				if (!killed_peds.contains(ped.GetHandle()) && !ped.IsPlayer() && ped.IsDead() && ENTITY::HAS_ENTITY_BEEN_DAMAGED_BY_ENTITY(ped.GetHandle(), Self::GetPed().GetHandle(), false))
+				{
+					killed_peds.insert(ped.GetHandle());
+					PAD::DISABLE_CONTROL_ACTION(0, 25, true);
+				}
+			}
+			running = false;
 		}
 
 		virtual void OnDisable() override

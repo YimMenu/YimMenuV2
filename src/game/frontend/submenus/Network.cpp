77290e1d1ac6a6492a45c0b9e246c7344435b971
ThePosiDong <kurdanstpolek@gmail.com>
2025-06-17 17:44:00 +0200

    Added disablephone to silencephonecalls

--- src/game/frontend/submenus/Network.cpp
+++ src/game/frontend/submenus/Network.cpp
@@ -77,11 +77,12 @@ namespace YimMenu::Submenus
 
 		enhancements->AddItem(std::make_shared<BoolCommandItem>("fastjoin"_J));
 		enhancements->AddItem(std::make_shared<BoolCommandItem>("disabledeathbarriers"_J));
-		enhancements->AddItem(std::make_shared<BoolCommandItem>("despawnbypass"_J)); // move this somewhere else?
+		enhancements->AddItem(std::make_shared<BoolCommandItem>("despawnbypass"_J));
 		enhancements->AddItem(std::make_shared<BoolCommandItem>("bypasscasinogeoblock"_J));
 		enhancements->AddItem(std::make_shared<BoolCommandItem>("forcescripthost"_J));
 		enhancements->AddItem(std::make_shared<BoolCommandItem>("pausegame"_J));
 		enhancements->AddItem(std::make_shared<BoolCommandItem>("nocalls"_J));
+		enhancements->AddItem(std::make_shared<ConditionalItem>("nocalls"_J, std::make_shared<BoolCommandItem>("disablephone"_J)));
 
 		session->AddItem(joinGroup);
 		session->AddItem(bountyGroup);

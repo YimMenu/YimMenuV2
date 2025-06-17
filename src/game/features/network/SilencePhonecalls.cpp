a80c88a9e72c4c124cdfe44441d6eacce99753b1
ThePosiDong <kurdanstpolek@gmail.com>
2025-06-17 18:22:17 +0200

    fixed SilencePhoneCalls

--- src/game/features/network/SilencePhonecalls.cpp
+++ src/game/features/network/SilencePhonecalls.cpp
@@ -10,10 +10,10 @@ namespace YimMenu::Features
 
 		virtual void OnTick() override
 		{
-			constexpr auto is_incoming_call = ScriptGlobal(22452);
-			constexpr auto is_phone_call_in_progress = ScriptGlobal(22448);
-			constexpr auto phone_call_state = ScriptGlobal(22442);
-			constexpr auto calling_character = ScriptGlobal(8235);
+			constexpr auto is_incoming_call = ScriptGlobal(22898);
+			constexpr auto is_phone_call_in_progress = ScriptGlobal(22608);
+			constexpr auto phone_call_state = ScriptGlobal(22602);
+			constexpr auto calling_character = ScriptGlobal(8391);
 
 			if (*Pointers.IsSessionStarted)
 			{

2e5d5e3d8d9327e3268edcedd14a011224631e42
ThePosiDong <kurdanstpolek@gmail.com>
2025-06-18 15:27:46 +0200

    Updated ClaimSafeEarnings.cpp

--- src/game/features/recovery/ClaimSafeEarnings.cpp
+++ src/game/features/recovery/ClaimSafeEarnings.cpp
@@ -40,37 +40,37 @@ namespace YimMenu::Features
 			case eAppVinewoodMenuSafe::NIGHTCLUB:
 			{
 				if (GPBD_FM::Get()->Entries[Self::GetPlayer().GetId()].PropertyData.NightclubData.SafeCashValue > 0)
-					*ScriptGlobal(2707980).As<BOOL*>() = TRUE;
+					*ScriptGlobal(2708201).As<BOOL*>() = TRUE;
 				break;
 			}
 			case eAppVinewoodMenuSafe::ARCADE:
 			{
 				if (GPBD_FM::Get()->Entries[Self::GetPlayer().GetId()].PropertyData.ArcadeData.SafeCashValue > 0)
-					*ScriptGlobal(2707989).As<BOOL*>() = TRUE;
+					*ScriptGlobal(2708210).As<BOOL*>() = TRUE;
 				break;
 			}
 			case eAppVinewoodMenuSafe::AGENCY:
 			{
 				if (GPBD_FM::Get()->Entries[Self::GetPlayer().GetId()].PropertyData.FixerHQData.SafeCashValue > 0)
-					*ScriptGlobal(2707998).As<BOOL*>() = TRUE;
+					*ScriptGlobal(2708219).As<BOOL*>() = TRUE;
 				break;
 			}
 			case eAppVinewoodMenuSafe::SALVAGE_YARD:
 			{
 				if (GPBD_FM::Get()->Entries[Self::GetPlayer().GetId()].PropertyData.SalvageYardData.TotalEarnings > 0)
-					*ScriptGlobal(2708007).As<BOOL*>() = TRUE;
+					*ScriptGlobal(2708252).As<BOOL*>() = TRUE;
 				break;
 			}
 			case eAppVinewoodMenuSafe::BAIL_OFFICE:
 			{
 				if (GPBD_FM::Get()->Entries[Self::GetPlayer().GetId()].PropertyData.BailShopData.SafeCashValue > 0)
-					*ScriptGlobal(2708016).As<BOOL*>() = TRUE;
+					*ScriptGlobal(2708252).As<BOOL*>() = TRUE;
 				break;
 			}
 			case eAppVinewoodMenuSafe::GARMENT_FACTORY:
 			{
 				if (GPBD_FM::Get()->Entries[Self::GetPlayer().GetId()].PropertyData.HackerDenData.SafeCashValue > 0)
-					*ScriptGlobal(2708025).As<BOOL*>() = TRUE;
+					*ScriptGlobal(2708240).As<BOOL*>() = TRUE;
 				break;
 			}
 			}

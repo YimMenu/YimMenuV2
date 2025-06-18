24e1aa810affb7eaa280958d6c83387aed88a842
ThePosiDong <kurdanstpolek@gmail.com>
2025-06-18 15:30:08 +0200

    Updated UnlockGTAPlus.cpp

--- src/game/features/recovery/UnlockGTAPlus.cpp
+++ src/game/features/recovery/UnlockGTAPlus.cpp
@@ -18,8 +18,8 @@ namespace YimMenu::Features
 		virtual void OnTick() override
 		{
 			*Pointers.HasGTAPlus = true;
-			*ScriptGlobal(1963591).As<bool*>() = true;
-			*ScriptGlobal(1963591).At(3).As<int*>() = (1 << 3) | (1 << 1);
+			*ScriptGlobal(1965683).As<bool*>() = true;
+			*ScriptGlobal(1965683).At(3).As<int*>() = (1 << 3) | (1 << 1);
 		}
 
 		virtual void OnDisable() override

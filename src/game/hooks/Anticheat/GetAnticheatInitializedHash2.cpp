dadcee1d8c3448c8bde1673f08a5ba74d5fa9181
Mr-X-GTA <110748953+Mr-X-GTA@users.noreply.github.com>
2025-07-04 11:37:49 +0200

    fix: fix BattlEyeServerProcessPlayerJoin

--- src/game/hooks/Anticheat/GetAnticheatInitializedHash2.cpp
+++ src/game/hooks/Anticheat/GetAnticheatInitializedHash2.cpp
@@ -8,7 +8,8 @@ namespace YimMenu::Hooks
 	std::uint32_t Anticheat::GetAnticheatInitializedHash2(void* ac_var, std::uint32_t seed)
 	{
 		auto orig = (*Pointers.AnticheatContext) ? (*Pointers.AnticheatContext)->m_BattlEyeEnabled : false;
-		(*Pointers.AnticheatContext)->m_BattlEyeEnabled = true; // integ checks will boot us out if we set this outside this function
+		if (*Pointers.AnticheatContext)
+			(*Pointers.AnticheatContext)->m_BattlEyeEnabled = true; // integ checks will boot us out if we set this outside this function
 		auto ret = BaseHook::Get<Anticheat::GetAnticheatInitializedHash2, DetourHook<decltype(&Anticheat::GetAnticheatInitializedHash2)>>()->Original()(ac_var, seed);
 		if (*Pointers.AnticheatContext)
 			(*Pointers.AnticheatContext)->m_BattlEyeEnabled = orig;

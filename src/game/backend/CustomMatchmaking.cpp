ed3384d5f5663d3349bd91675b67fc64d3e06224
tupoy-ya <tupoy-ya@users.noreply.github.com>
2026-01-16 17:38:13 +0500

    fix(CI): Potentially fix zig CI.
    Why is it even a thing?

--- src/game/backend/CustomMatchmaking.cpp
+++ src/game/backend/CustomMatchmaking.cpp
@@ -213,7 +213,7 @@ namespace YimMenu
 				if (Features::_SortMethod.GetState() != 0)
 				{
 					std::qsort(m_FoundSessions, m_NumSessionsFound, sizeof(Session), [](const void* a1, const void* a2) -> int {
-						std::strong_ordering result;
+						std::strong_ordering result(0);
 
 						if (Features::_SortMethod.GetState() == 1)
 						{

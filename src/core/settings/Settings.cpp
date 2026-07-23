e4ec6f87d146bea91ae36b673b4dfb66aa7b6a22
Mr_Bouquet <lukem4469@gmail.com>
2026-07-23 17:10:21 +1000

    Update Settings.cpp

--- src/core/settings/Settings.cpp
+++ src/core/settings/Settings.cpp
@@ -1,124 +1,123 @@
 #include "Settings.hpp"
 
-#include "IStateSerializer.hpp"
-#include "Settings.hpp"
+#include "core/commands/Commands.hpp"
+#include "core/commands/HotkeySystem.hpp"
+#include "core/commands/LoopedCommand.hpp"
+#include "game/backend/Self.hpp"
+#include "game/frontend/items/Items.hpp"
+#include "game/frontend/items/DrawHotkey.hpp"
+#include "game/frontend/submenus/Settings/LuaScripts.hpp"
+#include "game/frontend/submenus/Settings/GUISettings.hpp"
 
-
-namespace YimMenu
+namespace YimMenu::Submenus
 {
-	Settings::Settings() :
-	    m_SettingsFile(),
-	    m_StateSerializers(),
-	    m_InitialLoadDone(false)
+	// TODO: refactor this
+	static void Hotkeys()
 	{
-	}
+		ImGui::BulletText("Hold the button with the command name and enter a keystroke to change its hotkey");
+		ImGui::BulletText("If a command has an existing hotkey, clicking the button will remove it");
 
-	void Settings::InitializeImpl(File settingsFile)
-	{
-		m_SettingsFile = settingsFile;
-
-		if (!settingsFile.Exists())
-		{
-			Reset();
-			return;
-		}
-
-		std::ifstream file(m_SettingsFile);
-
-		try
-		{
-			file >> m_Json;
-			file.close();
-		}
-		catch (std::exception&)
-		{
-			LOG(WARNING) << "Detected corrupt settings, resetting settings...";
-			Reset();
-			return;
-		}
-
-		for (auto& serializer : m_StateSerializers)
-			LoadComponentImpl(serializer);
-
-		LOG(VERBOSE) << "All settings loaded";
-		m_InitialLoadDone = true;
-	}
-
-	void Settings::TickImpl()
-	{
-		std::lock_guard lock(m_Mutex);
-		while (!m_LateLoaders.empty())
-		{
-			if (auto component = std::move(m_LateLoaders.front()))
+		ImGui::Spacing();
+		ImGui::Separator();
+		ImGui::Spacing();
+		
+		// this assumes we can't add new commands in runtime, but a lot of other subsystems assume that too
+		static std::map<std::string, CommandLink*> sortedCommands;
+		static bool commandsSorted = []() {
+			for (auto& [hash, command] : Commands::GetCommands())
 			{
-				LoadComponent(component);
+				if (auto it = g_HotkeySystem.m_CommandHotkeys.find(hash); it != g_HotkeySystem.m_CommandHotkeys.end())
+					sortedCommands.emplace(command->GetLabel(), &it->second);
 			}
+			return true;
+		}();
 
-			m_LateLoaders.pop();
-		}
+		HotkeySystem::SetBeingModifed(false);
 
-		if (m_InitialLoadDone && ShouldSave())
+		for (auto& [name, link] : sortedCommands)
 		{
-			for (auto& serializer : m_StateSerializers)
-				if (serializer->IsStateDirty())
-					SaveComponentImpl(serializer);
-
-			std::ofstream file(m_SettingsFile, std::ios::out | std::ios::trunc);
-			file << m_Json.dump(4);
-			file.close();
+			if (name.empty())
+				continue;
+			DrawHotkey(link, name);
 		}
-	}
+	};
 
-	void Settings::AddComponentImpl(IStateSerializer* serializer)
+	Settings::Settings() :
+	#define ICON_FA_GEARS "\xef\x80\x93"
+	    Submenu::Submenu("Settings", ICON_FA_GEARS)
 	{
-		std::lock_guard lock(m_Mutex);
-		m_StateSerializers.push_back(serializer);
-		if (m_InitialLoadDone)
-			m_LateLoaders.push(serializer);
-	}
+		auto hotkeys = std::make_shared<Category>("Hotkeys");
+		auto gui = std::make_shared<Category>("GUI");
+		auto game = std::make_shared<Category>("Game");
 
-	void Settings::LoadComponentImpl(IStateSerializer* serializer)
-	{
-		if (!m_Json.contains(serializer->GetSerializerComponentName()) || !m_Json[serializer->GetSerializerComponentName()].is_object())
-			m_Json[serializer->GetSerializerComponentName()] = nlohmann::json::object();
+		auto uiStyle = std::make_shared<Group>("UI");
+		auto playerEsp = std::make_shared<Group>("Player ESP", 10);
+		auto pedEsp = std::make_shared<Group>("Ped ESP", 10);
+		auto objectEsp = std::make_shared<Group>("Object ESP");
+		auto overlay = std::make_shared<Group>("Overlay");
+		auto chat = std::make_shared<Group>("Chat");
+		auto hud = std::make_shared<Group>("HUD");
 
-		try
-		{
-			serializer->LoadState(m_Json[serializer->GetSerializerComponentName()]);
-		}
-		catch (const std::exception& e)
-		{
-			LOGF(FATAL, "Failed to load component {}: {}", serializer->GetSerializerComponentName(), e.what());
-		}
-	}
+		hotkeys->AddItem(std::make_shared<ImGuiItem>(Hotkeys));
 
-	void Settings::SaveComponentImpl(IStateSerializer* serializer)
-	{
-		try
-		{
-			serializer->SaveState(m_Json[serializer->GetSerializerComponentName()]);
-		}
-		catch (const std::exception& e)
-		{
-			LOGF(FATAL, "Failed to save component {}: {}", serializer->GetSerializerComponentName(), e.what());
-		}
-	}
+		// Players
+		uiStyle->AddItem(std::make_shared<ListCommandItem>("styleselector"_J));
 
-	void Settings::Reset()
-	{
-		std::ofstream file(m_SettingsFile, std::ios::out | std::ios::trunc);
-		file << "{}" << std::endl;
-		file.close();
-		m_Json.clear();
-		m_InitialLoadDone = true;
-	}
+		playerEsp->AddItem(std::make_shared<BoolCommandItem>("espdrawplayers"_J));
+		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<BoolCommandItem>("espdrawdeadplayers"_J)));
 
-	bool Settings::ShouldSave()
-	{
-		for (auto& serializer : m_StateSerializers)
-			if (serializer->IsStateDirty())
-				return true;
+		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<BoolCommandItem>("espnameplayers"_J, "Player Name")));
+		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<ColorCommandItem>("namecolorplayers"_J)));
 
-		return false;
+		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<BoolCommandItem>("espdistanceplayers"_J, "Player Distance")));
+
+		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<BoolCommandItem>("espskeletonplayers"_J, "Player Skeleton")));
+		playerEsp->AddItem(std::make_shared<ConditionalItem>("espdrawplayers"_J, std::make_shared<ColorCommandItem>("skeletoncolorplayers"_J)));
+
+		// Peds
+		pedEsp->AddItem(std::make_shared<BoolCommandItem>("espdrawpeds"_J));
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espdrawdeadpeds"_J)));
+
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espmodelspeds"_J, "Ped Hashes")));
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<ColorCommandItem>("hashcolorpeds"_J)));
+
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espnetinfopeds"_J, "Ped Net Info")));
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espscriptinfopeds"_J, "Ped Script Info")));
+
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espdistancepeds"_J, "Ped Distance")));
+
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<BoolCommandItem>("espskeletonpeds"_J, "Ped Skeleton")));
+		pedEsp->AddItem(std::make_shared<ConditionalItem>("espdrawpeds"_J, std::make_shared<ColorCommandItem>("skeletoncolorpeds"_J)));
+
+		objectEsp->AddItem(std::make_shared<BoolCommandItem>("espdrawobjects"_J));
+		objectEsp->AddItem(std::make_shared<ConditionalItem>("espdrawobjects"_J, std::make_shared<ColorCommandItem>("hashcolorobjects"_J)));
+		objectEsp->AddItem(std::make_shared<ConditionalItem>("espdrawobjects"_J, std::make_shared<BoolCommandItem>("espnetinfoobjects"_J, "Object Net Info")));
+		objectEsp->AddItem(std::make_shared<ConditionalItem>("espdrawobjects"_J, std::make_shared<BoolCommandItem>("espscriptinfoobjects"_J, "Object Script Info")));
+
+		objectEsp->AddItem(std::make_shared<ConditionalItem>("espdrawobjects"_J, std::make_shared<BoolCommandItem>("espdistanceobjects"_J, "Object Distance")));
+
+
+		overlay->AddItem(std::make_shared<BoolCommandItem>("overlay"_J));
+		overlay->AddItem(std::make_shared<ConditionalItem>("overlay"_J, std::make_shared<BoolCommandItem>("overlayfps"_J)));
+
+		chat->AddItem(std::make_shared<BoolCommandItem>("clearchat"_J));
+
+		hud->AddItem(std::make_shared<BoolCommandItem>("hidehud"_J));
+		hud->AddItem(std::make_shared<BoolCommandItem>("disabletextsounds"_J));
+
+		game->AddItem(playerEsp);
+		game->AddItem(pedEsp);
+		game->AddItem(objectEsp);
+
+		gui->AddItem(uiStyle);
+		gui->AddItem(overlay);
+		gui->AddItem(chat);
+		gui->AddItem(hud);
+
+		AddCategory(std::move(hotkeys));
+		AddCategory(std::move(gui));
+		AddCategory(std::move(game));
+		AddCategory(DrawGUISettingsMenu());
+		AddCategory(BuildLuaScriptsMenu());
 	}
 }

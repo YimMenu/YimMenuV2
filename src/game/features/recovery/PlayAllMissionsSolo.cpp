ccd1a7ed5e34479436381108027a506ac4b1522b
Godred <adltyhkak0nn1@gmail.com>
2026-07-29 21:35:51 +0900

    Enable solo casino heist missions with new patch
    Added a patch to enable solo casino heist missions.

--- src/game/features/recovery/PlayAllMissionsSolo.cpp
+++ src/game/features/recovery/PlayAllMissionsSolo.cpp
@@ -151,6 +151,15 @@ namespace YimMenu::Features
 				m_QuickRestartPatch2 = ScriptPatches::AddPatch("fm_mission_controller"_J, ScriptPointer("QuickRestartPatch2", "5A 47 00 5D ? ? ? 5D ? ? ? 75"), {0x2B, 0x00, 0x00});
 			}
 			m_QuickRestartPatch2->Enable();
+
+			if (!m_casinosolo)
+			{
+				m_casinosolo = ScriptPatches::AddPatch("fmmc_launcher"_J, ScriptPointer("casinosolo", "2D 01 03 00 00 5D ? ? ? 2A 06 56 05 00 5D ? ? ? 20 2A 06 56 05 00 5D").Add(5), {0x71, 0x2E, 0x01, 0x01});
+			}
+			m_casinosolo->Enable();
+			// By this patch, you can use the finale board of casino heist, and can change outfits or teams in missions like Lowriders
+		}
+
 		}
 
 		virtual void OnTick() override
@@ -258,6 +267,10 @@ namespace YimMenu::Features
 			if (m_QuickRestartPatch2)
 			{
 				m_QuickRestartPatch2->Disable();
+			}
+			if (m_casinosolo)
+			{
+				m_casinosolo->Disable();
 			}
 		}
 	};

073a03a8c0362bbee6a327b3ce12a9d43edcee4d
Mr_Bouquet <lukem4469@gmail.com>
2026-08-01 13:57:39 +1000

    Update Self.cpp

--- src/game/frontend/submenus/Self.cpp
+++ src/game/frontend/submenus/Self.cpp
@@ -30,7 +30,6 @@ namespace YimMenu::Submenus
 		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("keepplayerclean"_J));
 		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("disablecriticalhits"_J));
 		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("hidehud"_J));
-		globalsGroup->AddItem(std::make_shared<BoolCommandItem>("disabletextsounds"_J));
 
 		toolsGroup->AddItem(std::make_shared<CommandItem>("skipcutscene"_J));
 		toolsGroup->AddItem(std::make_shared<CommandItem>("skipconversation"_J));
